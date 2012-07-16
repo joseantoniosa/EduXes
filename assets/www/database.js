79797b5dc74b37c3b768d7c1fa17641bdb0abb39
Jose Antonio S.A <jose.antonio.sa@gmail.com>
2012-07-16 20:41:46 +0200

    First list of groups

--- assets/www/database.js
+++ assets/www/database.js
@@ -3,26 +3,42 @@
 
     function errorCB(err) {
  	   console.log("Error processing SQL: "+err.code);
-
-        alert("Error processing SQL: "+err.code);
+ 	   alert("Error processing SQL: "+err.code);
     }
 
     function successCB() {
 //        alert("success!");
-  	   console.log("Success ");
+    	console.log("Success ");
     }
 
     function populateDB(tx) {
-        tx.executeSql('DROP TABLE IF EXISTS GROUPS');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS GROUPS (id unique, data)');
-        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (1, "First row")');
-        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (2, "Second row")');
-        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (3, "Third row")');
-        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (4, "Fourth row")');
+    	tx.executeSql('DROP TABLE IF EXISTS GROUPS');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS GROUPS (id  integer primary key , data)');
+        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (NULL, "First group  1")');
+        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (NULL, "Second group 2")');
+        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (NULL, "Third group  3" )');
+        tx.executeSql('INSERT INTO GROUPS (id, data) VALUES (NULL, "Fourth group 4")');
+        
+    	tx.executeSql('DROP TABLE IF EXISTS STUDENTS');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS STUDENTS (id integer primary key, data)');
+        tx.executeSql('INSERT INTO STUDENTS (id, data) VALUES (NULL, "First student")');
+        tx.executeSql('INSERT INTO STUDENTS (id, data) VALUES (NULL, "Second student")');
+        tx.executeSql('INSERT INTO STUDENTS (id, data) VALUES (NULL, "Third student")');
+        tx.executeSql('INSERT INTO STUDENTS (id, data) VALUES (NULL, "Fourth student")');
+        
+        
    }
 
    function queryDB(tx) {
 	    tx.executeSql('SELECT * FROM GROUPS', [], querySuccess, errorCB);
+	}
+   function queryGroupsDB(tx) {
+	    tx.executeSql('SELECT * FROM GROUPS', [], querySuccess, errorCB);
+	}
+
+   
+   function queryStudentsDB(tx) {
+	    tx.executeSql('SELECT * FROM STUDENTS', [], queryStudentsSuccess, errorCB);
 	}
 
    function querySuccess(tx, results) {
@@ -38,9 +54,40 @@
 	   console.log("Number of rows inserted: " +  len)
 // How to populate a List ?
 	   for (var i=0;i<len;i++) {
-		   $('#actionList').append("<li>"+results.rows.item(i).data +"</li>");
+		   $('#grupos_ul').append("<li>"+results.rows.item(i).data +"</li>");
 		   
 	   }
+	   $('#grupos_ul').listview('refresh');
+	   
+   }
+   
+   
+   function queryStudentsSuccess(tx, results) {
+	   console.log("Returned rows = " + results.rows.length);
+	   var len = results.rows.length;
+	   // this will be true since it was a select statement and so rowsAffected was 0
+//	   if (!resultSet.rowsAffected) {
+//		   console.log('No rows affected!');
+//		   return false;
+//	   }
+	   // for an insert statement, this property will return the ID of the last inserted row
+	   console.log("Last inserted row ID = " + results.insertId);
+	   console.log("Number of rows inserted: " +  len)
+// How to populate a List ?
+	   for (var i=0;i<len;i++) {
+		   $('#students_ul').append("<li>"+results.rows.item(i).data +"</li>");		  
+	   }
+	   $('#students_ul').listview('refresh');
+	   
+   }
+   
+   function loadGroups()
+   {
+	   
+   }
+
+   function loadStudents()
+   {
 	   
    }
 
--- assets/www/interface.js
+++ assets/www/interface.js
@@ -1,28 +1,24 @@
 // Referencia á base de datos
 var db;
 
-function onDeviceReady() {
-	console.log("Hello World\n");
-	
-//	$("#inicio").html("<b>Capa inicial</b>");// Works!
-	
+function onDeviceReady() {	
+
 	var db = window.openDatabase("eduxesdb", "1.0", "Gestion de Aula", 200000);
 
-	$("#etiqueta1").val("Alo France 02"); //results.rows.item(i).data);
-	$("#dentro_inicio").html("Alo France 2"); //results.rows.item(i).data);
-	console.log("One "+ db +"\n");
+//	$("#etiqueta1").val("Alo France 02"); //results.rows.item(i).data);
+//	$("#dentro_inicio").html("Alo France 2"); //results.rows.item(i).data);
+//	console.log("One "+ db +"\n");
 	
 	db.transaction(populateDB, errorCB, successCB);
-	db.transaction(queryDB, errorCB, successCB);
 	
-	
+	db.transaction(queryGroupsDB, errorCB, successCB);
+	loadGroups();
+	loadStudents();
 //    loadCards();
 }
 
 function init(){
 	console.log("Hello World 0\n");
-//	$("#inicio").html("<b>Capa inicial</b>"); // works!
-	
+//	$("#inicio").html("<b>Capa inicial</b>"); // works!	
 	document.addEventListener("deviceready", onDeviceReady, false);
-	console.log("Hello World 1 ");
 }
