62ad233d303b95b252d6db86ee7b6b6263d7233c
Jose Antonio S.A <jose.antonio.sa@gmail.com>
2012-12-24 06:26:37 +0100

    Master Memory. Continue with working. Minor change. Methodology(6)

--- informal_doc/LaTeX/Aux/database.js
+++ informal_doc/LaTeX/Aux/database.js
@@ -1,15 +1,15 @@
 
 \emph{ errorCB() }     & Error handler \\
 \emph{  successCB() &  Error handler \\
-\emph{ queryDB(tx) } & List groups \footnote{Deprecated} \\
 
 \emph { queryAllStudentsSuccess() & List filled with students information \\
-\emph { queryAllStudentsDB(tx) } & List filled with students information \\
+\emph { queryAllStudentsDB() } & List filled with students information \\
 \emph { queryStudentSuccess() } & Fill Editor Page with student information \\
 \emph { queryStudentDB() } & Fill Editor Page with student information \\
 \emph { queryNewStudentDB() } & Fill Editor Page with student information. Group list \\
 \emph { queryStudentsSuccess() } & Fill list of students with name, surname and photo  \\
 \emph { queryStudentsDB() } & Fill list of students with name, surname and photo  \\
+\emph { loadStudents() { }
 
 \emph { queryStudentsByGroupSuccess() }& Settings->Group-> List of Students \\
 
@@ -21,6 +21,9 @@
 
 \emph { queryReportAttendanceDB() } & report->Attendance->Group \\
 \emph { queryReportAttendanceSuccess() }& report->Attendance->Group \\
+\emph { reportAttendanceDB(db) {  Reports -> Attendance }\\
+
+
 
 \emph { loadGroupsAssessment() } Report->Assessment ->  Group\\
 
@@ -30,132 +33,90 @@
 
 \emph {  fillSelectStudent() } {  Fill attendance student page with options } \\
 
-\emph { queryStudentsAttendanceSuccess(tx, results) {  <!-- Third Window: List of Students.  Attendance //-->
+\emph { queryStudentsAttendanceSuccess() {   Third Window: List of Students.  Attendance }\\
 
-\emph { queryStudentsAttendanceDB(tx) { <!-- Third Window: List of Students.  Attendance //-->
+\emph { queryStudentsAttendanceDB() {  Third Window: List of Students.  Attendance }\\
+\emph { loadStudentAttendance(db) { } \\
 
 
-\emph { insertNewGroup(db, name, other_data)} { Insert Group }   \\
-\emph { updateGroup(db, name, other_data)}{  Update Group  }\\
-\emph { deleteGroup(db) }{ Delete Group } \\
+
+\emph { insertNewGroup()} { Insert Group }   \\
+\emph { updateGroup()}{  Update Group  }\\
+\emph { deleteGroup() }{ Delete Group } \\
   // 
-\emph { loadGroup(db, id_group){ Load only one group! } \\
+\emph { loadGroup(){ Load only one group! } \\
 
-\emph { queryAllGroupsDB(tx) { List all groups }  \\
-\emph { loadAllGroups(db) { { List all groups }  \\
+\emph { queryAllGroupsDB() { List all groups }  \\
+\emph { loadAllGroups() { { List all groups }  \\
 
 
-// ---------------------------------------------------------------------------------------------------------
-//
-//      STUDENTS    Management:
-//
-// ----------------------------------------------------------------------------------------------------------
     // Insert new Student
-\emph { insertNewStudent() } { Insert new Student  } \\
+\emph { insertNewStudent() } { Settings -> Insert new Student  } \\
 
-\emph { querySaveStudentDB (tx){ // Update Student \\
-\emph { deleteStudent(db) {
+\emph { querySaveStudentDB (){ Settings -> Update Student \\
+\emph { saveStudent(){ }
+\emph { deleteStudent() { Settings -> Delete Student }
 
-// Low level insert
-\emph { insertStudentStateL(db, id_student, id_group, id_session, state, actual_date ){ Insert New Student 
-// Low level update
-\emph { updateStudentStateL(db, id_student, id_group, id_session, state, actual_date ) { Update Student 
+\emph { insertStudentStateL( ){ Settings -> Insert New Student 
+\emph { updateStudentStateL( ) { Settings ->  Update Student 
 
 // Check whether student state changes
 // @id_student student id
-\emph { stateCheck(db, id_student, id_group, id_session, state, actual_date){
+\emph { stateCheck( ){
 // Write Student state
-\emph { updateStudentState(db, id_student, id_group, id_session, state, actual_date ){
-    exist = stateCheck(db, id_student, id_group, id_session, state, actual_date); // Is asynchronous
-}
+\emph { updateStudentState( ){ } \\
 
 //
-// Save Student info
-   \emph { saveStudent(db){
-       db.transaction(querySaveStudentDB);
-   }
 
-   \emph { loadStudents(db) {
-       db.transaction(queryStudentsDB);
-   }
    // Load only one student!
-   \emph { loadStudent(db){
-       db.transaction(queryStudentDB);
-   }
+\emph { loadStudent(db){
+\emph { loadNewStudent(){
 
+\emph { loadAllStudents() {
 
-   \emph { loadNewStudent(db){
-       db.transaction(  queryNewStudentDB);
-   }
+\emph { loadStudentsByGroup() { List Students by Group }
 
-   \emph { loadAllStudents(db) {
-          db.transaction(queryAllStudentsDB);
-   }
+\emph { queryLoadAllActivitiesDB() { Settings -> Show Activities (to be updated)
+\emph { loadAllActivities() { Settings -> Show Activities (to be updated)
 
-   \emph { loadStudentsByGroup(db,id_group) { List Students by Group }
-///---------------------------------------------------------------------------------------------------------
-//
-//      Activities
-//
-// ----------------------------------------------------------------------------------------------------------
-
-
-\emph { queryLoadAllActivitiesDB(tx) { Settings -> Show Activities (to be updated)
-\emph { loadAllActivities(db) { Settings -> Show Activities (to be updated)
-
-\emph { loadActivity(db, id_activity ) {Settings -> List Activities -> Load activity information
-
-
+\emph { loadActivity( ) {Settings -> List Activities -> Load activity information
 \emph { updateActivity(  ){Settings -> Activities -> List-> Load activity -> Update Activity. }\\
 
 //
 // Update Activities for each group
-\emph { updateActivitiesGroup(db, id_group, id_activity, enabled , a_date, notes){ Update Activities for each group
+\emph { updateActivitiesGroup( ){ Update Activities for each group
 
 
-\emph { insertActivitiesGroup(db, id_group, last_inserted_row, enabled , a_date, notes){ Update Activities for each group
+\emph { insertActivitiesGroup( ){ Update Activities for each group
 
-\emph {  insertNewActivity( d ) { {Settings -> Activities -> New 
+\emph {  insertNewActivity(  ) { {Settings -> Activities -> New 
 
-\emph { loadGroupsActivitiesEdit(db){ {Settings -> Activities -> New (List groups)
+\emph { loadGroupsActivitiesEdit( ){ {Settings -> Activities -> New (List groups)
 
-//
 //------------- Assessment:
+\emph { fillSelectStudentAssessment( ) { Assessment -> Student -> List options and get saved data.
 
-\emph { fillSelectStudentAssessment(db, select_student_id, id_session, id_student) { Assessment -> Student -> List options and get saved data.
-
-
-\emph { loadGroupAssessment(db,id_group) { Assessment -> Load group information }
-\emph { listStudentsByGroupAssessment(db, id_group, students_ul) { // Assessment. List Students, load data  from DB
+\emph { loadGroupAssessment(  ) { Assessment -> Load group information }
+\emph { listStudentsByGroupAssessment( ) { // Assessment. List Students, load data  from DB
 \emph { refreshGroupAssessment(){ ^^ ID.}
 
-\emph { updateStudentAssessmentL(db,id_student,id_group, id_activity,mark){  Update Student Assessment
+\emph { updateStudentAssessmentL(){  Update Student Assessment
 
-\emph { insertStudentAssessmentL(db,id_student,id_group, id_activity,mark) { Insert Student Assessment  
+\emph { insertStudentAssessmentL() { Insert Student Assessment  
 
-\emph { onChangeStudentAssessment(id_student,id_group, id_activity){ Select Update or Insert 
-
-
-
-\emph { queryListMaxActivities(tx) { List Number of activities }
-\emph { listMaxActivities(db) { List number of activities }
+\emph { onChangeStudentAssessment(){ Select Update or Insert 
 
 
-   \emph { deleteRawRecord(db, table, id){ Delete any row from any table }
+
+\emph { queryListMaxActivities() { List Number of activities }
+\emph { listMaxActivities( ) { List number of activities }
+
+
+\emph { deleteRawRecord( ){ Delete any row from any table }
 
 \emph { queryGroupsAttendanceDB() } & Reports -> Attendance ->List  groups \\
- \emph { loadGroupsAttendance(db) { Reports -> Attendance -> List  groups  } \\
-//
+\emph { loadGroupsAttendance(db) { Reports -> Attendance -> List  groups  } \\
 
-/*
- * For Students Attendance Table
- */
-    \emph { reportAttendanceDB(db) {
-       db.transaction(queryReportAttendanceDB);
-   }
 
-   \emph { loadStudentAttendance(db) {
-       db.transaction(queryStudentsAttendanceDB);
-   }
 
 
